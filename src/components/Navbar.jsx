89a3f28e9f40d8d4aefaca07c265e2fb7a17bcc9
Alexander Jackson <alexleejack04@gmail.com>
2025-03-16 23:39:55 -0400

    Navbar Layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,13 @@ function Navbar() {
 
     return (
       <>
-      
+        <nav className="m-8 flex flex-row justify-between">
+            <h2>Alexander J.</h2>
+            <ul className="flex flex-row justify-evenly w-xs">
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
       </>
     )
   }
