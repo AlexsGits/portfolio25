a3e79d844753f58e1331bc92a2d620a8bb7f2fc3
Alexander Jackson <alexleejack04@gmail.com>
2025-03-18 00:06:24 -0400

    Navbar Revamp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
+
 function Navbar() {
 
     return (
       <>
-        <nav className="m-8 flex flex-row justify-between">
-            <h2>Alexander J.</h2>
-            <ul className="flex flex-row justify-evenly w-xs">
-                <li>Projects</li>
-                <li>Contact</li>
+        <nav className="m-8 flex flex-row justify-between opacity-100">
+            <h2 className="text-lg font-semibold cursor-pointer">ALEXANDER J.</h2>
+            <ul className="flex flex-row justify-evenly w-3xs text-base">
+                <li className="hover:text-sky-400 cursor-pointer">Projects</li>
+                <li className="hover:text-sky-400 cursor-pointer">Contact</li>
             </ul>
         </nav>
       </>
